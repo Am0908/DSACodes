00e66497bee3a274cff3d17fd0aacc2c0b3a487d
Amrita <as09082020@gmail.com>
2021-12-31 19:40:05 +0530

    Create iterativesearch.c

---
+++ BST/iterativesearch.c
@@ -0,0 +1,13 @@
+struct Node *isearch(struct Node *t,int key)
+{
+    while(t!=NULL)
+    {
+        if(key==t->data)
+        return t;
+        else if(key<t->data)
+        t=t->lchild;
+        else
+        t=t->rchild;
+    }
+    return NULL;
+}
