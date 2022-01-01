e6d11e015af926c27cdb4a714fb2996dd66494bc
Amrita <as09082020@gmail.com>
2022-01-01 22:17:32 +0530

    Update bubble_sort.cpp

--- sorting techniques/bubble_sort.cpp
+++ sorting techniques/bubble_sort.cpp
@@ -2,6 +2,28 @@
 
 #include <iostream>
 using namespace std;
+
+void swap(int *x,int *y)
+{
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+void bubblesort(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(&arr[j],&arr[j+1]);
+            }
+
+        }
+    }
+}
 int main()
 {
     int n;
@@ -11,22 +33,10 @@ int main()
     {
         cin>>arr[i];
     }
-    for(int i=0;i<n-1;i++)
-    {
-        for(int j=0;j<n-1-i;j++)
-        {
-            if(arr[j]>arr[j+1])
-            {
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-            }
-
-        }
-    }
+    bubblesort(arr,n);
     for(int i=0;i<n;i++)
     {
-        cout<<arr[i];
+        cout<<arr[i]<<" ";
     }
     return 0;
 }
