bda800080df37400437dec5c600b9ad70401c6cc
Amrita <as09082020@gmail.com>
2022-01-14 00:37:56 +0530

    Update tree using vectors.cpp

--- CN Trees/tree using vectors.cpp
+++ CN Trees/tree using vectors.cpp
@@ -15,12 +15,57 @@ class treenode
     }
 };
 
+treenode<int>* takeinput()
+{
+    int rootdata;
+    cout<<"enter data:";
+    cin>>rootdata;
+    treenode<int>* root= new treenode<int>(rootdata);
+
+    int n;
+    cout<<"enter number of children:";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        treenode<int>* child = takeinput();
+        root->children.push_back(child);
+    }
+    return root;
+}
+
+void printree(treenode<int>*root)
+{
+    if(root==NULL)  //edge case as we will not pass a NULL root node.
+    {
+        cout<<"no nodes are present.";
+        return;
+    }
+    cout<<root->data<<": ";
+
+    for(int i=0;i<root->children.size();i++)
+    cout<<root->children[i]->data<<", ";
+    cout<<endl;
+    for(int i=0;i<root->children.size();i++)
+    printree(root->children[i]);
+}
+
 int main()
 {
-    treenode<int>* root= new treenode<int>(1);
-    treenode<int>* node1= new treenode<int>(2);
-    treenode<int>* node2= new treenode<int>(4);
-    root->children.push_back(node1);
-    root->children.push_back(node2);
+    /* treenode<int>* root= new treenode<int>(1);
+       treenode<int>* node1= new treenode<int>(2);
+       treenode<int>* node2= new treenode<int>(4);
+       root->children.push_back(node1);
+       root->children.push_back(node2); */
+
+
+    //take input.
+    treenode<int>* root= takeinput();
+
+    //display the nodes.
+    printree(root);
+
+    //TODO delete the tree.
+
+
     return 0;
 }
