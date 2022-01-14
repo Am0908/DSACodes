8f76d2274469577cd5f335a646df94e8268934b9
Amrita <as09082020@gmail.com>
2022-01-14 23:59:51 +0530

    Update avltree.c

--- AVLTrees/avltree.c
+++ AVLTrees/avltree.c
@@ -1,27 +1,97 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 struct node
 {
     struct node *lchild;
     int data;
     int height;
+    int bf;
     struct node *rchild;
 }*root=NULL;
 
 int nodeheight(struct node *p)
 {
     int hl,hr;
-    hl=p && p->lchild ? lchild->height : 0;
-    hr=p && p->rchild ? rchild->height : 0;
+    hl=p && p->lchild ? p->lchild->height : 0;
+    hr=p && p->rchild ? p->rchild->height : 0;
     return hl>hr?hl+1:hr+1;
 }
 
 int balancefactor(struct node *p)
 {
     int hl,hr;
-    hl=p && p->lchild ? lchild->height : 0;
-    hr=p && p->rchild ? rchild->height : 0;
+    hl=p && p->lchild ? p->lchild->height : 0;
+    hr=p && p->rchild ? p->rchild->height : 0;
     return hl-hr;
+}
+
+struct node *LLRotation(struct node *p)
+{
+    struct node *pl=p->lchild;
+    struct node *plr=p->rchild;
+
+    pl->rchild=p;
+    p->lchild=plr;
+    p->height=nodeheight(p);
+    pl->height=nodeheight(pl);
+
+    if(root==p)
+    root=pl;
+    return pl;
+}
+
+struct node *LRRotation(struct node *p)
+{
+    struct node *pl=p->lchild;
+    struct node *plr=pl->rchild;
+    pl->rchild=plr->lchild;
+    p->lchild=plr->rchild;
+    plr->lchild=pl;
+    plr->rchild=p;
+
+    pl->height=nodeheight(pl);
+    p->height=nodeheight(p);
+    plr->height=nodeheight(plr);
+
+    if(root==p)
+    root=plr;
+
+    return plr;
+}
+
+struct node *RRRotation(struct node *p)
+{
+ int lbf,rbf;
+ struct node *pr=p->rchild;
+ pr->bf=0;
+ p->rchild=pr->lchild;
+ pr->lchild=p;
+ lbf=nodeheight(p->lchild)+1;
+ rbf=nodeheight(p->rchild)+1;
+ p->bf=lbf-rbf;
+ if(p==root)root=pr;
+ return pr;
+}
+struct node *RLRotation(struct node *p)
+{
+ int lbf,rbf;
+ struct node *pr=p->rchild;
+ struct node *prl=pr->lchild;
+ prl->bf=0;
+
+ p->rchild=prl->lchild;
+ pr->lchild=prl->rchild;
+ prl->rchild=pr;
+ prl->lchild=p;
+ lbf=nodeheight(p->lchild)+1;
+ rbf=nodeheight(p->rchild)+1;
+ p->bf=lbf-rbf;
+
+ lbf=nodeheight(pr->lchild)+1;
+ rbf=nodeheight(pr->rchild)+1;
+ pr->bf=lbf-rbf;
+ if(p==root)root=prl;
+ return prl;
 }
 
 struct node *recursiveinsert(struct node *p,int key)
@@ -54,9 +124,21 @@ struct node *recursiveinsert(struct node *p,int key)
     return p;
 }
 
+void Inorder(struct node *p)
+{
+ if(p)
+ {
+ Inorder(p->lchild);
+ printf("%d ",p->data);
+ Inorder(p->rchild);
+ }
+}
+
 int main()
 {
-    printf("Hello World");
-
+    root=recursiveinsert(root,20);
+    root=recursiveinsert(root,10);
+    root=recursiveinsert(root,5);
+    Inorder(root);
     return 0;
 }
