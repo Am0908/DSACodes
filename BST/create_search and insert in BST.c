5e2b65f70d2a8d26798ddfd0d3ab495d10b2443a
Amrita <as09082020@gmail.com>
2022-01-16 16:01:01 +0530

    Update create_search and insert in BST.c

--- BST/create_search and insert in BST.c
+++ BST/create_search and insert in BST.c
@@ -3,65 +3,41 @@
 
 struct node
 {
-    struct node *lchild;
     int data;
+    struct node *lchild;
     struct node *rchild;
 }*root=NULL;
 
-void insert(int key)
+void iinsert(int x)
 {
-    struct node *t=root;
-    struct node *r,*p;
+    struct node *t=root,*p,*r=NULL;
     if(root==NULL)
     {
         p=(struct node *)malloc(sizeof(struct node));
-        p->data=key;
+        p->data=x;
         p->lchild=p->rchild=NULL;
         root=p;
-        return;
+        return ;
     }
     while(t!=NULL)
     {
         r=t;
-        if(key<t->data)
+        if(t->data==x)
+        return ;
+        else if(x<t->data)
         t=t->lchild;
-        else if(key>t->data)
-        t=t->rchild;
         else
-        return;
+        t=t->rchild;
     }
-    p=(struct node*)malloc(sizeof(struct node));
-    p->data=key;
+    p=(struct node *)malloc(sizeof(struct node));
+    p->data=x;
     p->lchild=p->rchild=NULL;
-
-    if(key<r->data) r->lchild=p;
-    else r->rchild=p;
+    if(p->data<r->data)
+    r->lchild=p;
+    else
+    r->rchild=p;
 }
 
-struct node *search(int key)
-{
-    struct node *t=root;
-    while(t!=NULL)
-    {
-        if(key==t->data)
-        return t;
-        else if(key<t->data)
-        t=t->lchild;
-        else
-        t=t->rchild;
-    }
-    return NULL;
-}
-
-void Inorder(struct node *p)
-{
-    if(p)
-    {
-        Inorder(p->lchild);
-        printf("%d ",p->data);
-        Inorder(p->rchild);
-    }
-}
 struct node *recursiveinsert(struct node *p,int key)
 {
     struct node *t;
@@ -80,21 +56,55 @@ struct node *recursiveinsert(struct node *p,int key)
 
     return p;
 }
+
+struct node *isearch(int key)
+{
+    struct node *t=root;
+    while(t!=NULL)
+    {
+        if(t->data==key)
+        return t;
+        else if(key<t->data)
+        t=t->lchild;
+        else
+        t=t->rchild;
+    }
+    return NULL;
+}
+
+
+struct node *rsearch(struct node *p,int key)
+{
+    if(p==NULL)
+    return NULL;
+    if(p->data==key)
+    return p;
+    else if(key<p->data)
+    return rsearch(p->lchild,key);
+    else
+    return rsearch(p->rchild,key);
+}
+
+void inorder(struct node *p)
+{
+    if(p)
+    {
+        inorder(p->lchild);
+        printf("%d ",p->data);
+        inorder(p->rchild);
+    }
+}
+
 int main()
 {
-    struct node *temp;
-    insert(10);
-    insert(5);
-    insert(20);
-    insert(8);
-    insert(30);
-    printf("tree elements:");
-    Inorder(root);
-    printf("\n");
-    temp=search(50);
-    if(temp!=NULL)
-    printf("element %d found!",temp->data);
+    iinsert(10);
+    iinsert(20);
+    iinsert(30);
+    iinsert(25);
+    inorder(root);
+    if(rsearch(root,20))
+    printf("Found");
     else
-    printf("element is not found!");
+    printf("Not Found");
     return 0;
 }
