62e094963e5422f9332f46acb839b2b836394fc0
Amrita <as09082020@gmail.com>
2022-02-03 20:21:11 +0530

    Update k smallest elements.cpp

--- Heap / priority queue/k smallest elements.cpp
+++ Heap / priority queue/k smallest elements.cpp
@@ -6,17 +6,24 @@ void ksmallestelements(int arr[], int n, int k)
 {
     priority_queue<int> pq;
 
-    for(int i = 0; i < k; i++)
+    // for(int i = 0; i < k; i++)
+    // {
+    //     pq.push(arr[i]);
+    // }
+    // for(int i = k; i < n; i++)
+    // {
+    //     if(arr[i] < pq.top())
+    //     {
+    //         pq.push(arr[i]);
+    //         pq.pop();
+    //     }
+    // }
+
+    for(int i=0; i<n; i++)
     {
         pq.push(arr[i]);
-    }
-    for(int i = k; i < n; i++)
-    {
-        if(arr[i] < pq.top())
-        {
-            pq.push(arr[i]);
-            pq.pop();
-        }
+        if(pq.size()>k)
+        pq.pop();
     }
 
     while(!pq.empty())
