d668a8c912ea35d2a1b4ef07067399a6f1244570
amrita <as09082020@gmail.com>
2022-02-16 22:26:20 +0530

    Create nth fibonacci number.cpp

---
+++ Dynamic Programming/nth fibonacci number.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int fibo_helper(int* ans, int n)
+{
+    if(n<=1)
+    return n;
+
+    //chcek if output already exists
+    if(ans[n] != -1)
+    return ans[n];
+
+    int a = fibo_helper(ans, n-1);
+    int b = fibo_helper(ans, n-2);
+
+    //save ans for future use
+    ans[n] = a + b;
+
+
+    //return the final answer
+    return ans[n];
+}
+
+int fibo(int n)
+{
+    int* ans = new int[n+1];
+    for(int i=0;i<=n;i++)
+    ans[i] = -1;
+
+    return fibo_helper(ans, n);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    cout<<"Required fibonacci number: ";
+    cout<<fibo(n);
+    return 0;
+}
