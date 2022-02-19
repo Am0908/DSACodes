5e52de6da5e79d1626ac642e2a33413c2f078c89
amrita <as09082020@gmail.com>
2022-02-19 21:33:10 +0530

    Create Number of balanced Binary Trees using recursion.cpp

---
+++ Dynamic Programming/Number of balanced Binary Trees using recursion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int balancedbst(int h)
+{
+    if(h<=1)
+    return 1;
+
+    int x = balancedbst(h-1);
+    int y = balancedbst(h-2);
+
+    int ans = x * x + 2 * x * y;
+    return ans;
+}
+
+int main()
+{
+    int height;
+    cin>>height;
+    cout<<balancedbst(height);
+    return 0;
+}
